eac2e6018be7c0a10451b985892893ef5a64aa47
Debabrata <m.debabrata100@gmail.com>
2025-12-11 06:32:04 +0530

    Fix: Order was not saved while listening payment complete event

--- orders/src/events/listeners/payment-created-listener.ts
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -12,13 +12,15 @@ export class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
   readonly subject = Subjects.PaymentCreated;
   queueGroupName = queueGroupName;
   async onMessage(data: PaymentCreatedEvent['data'], msg: Message) {
-    const order = Order.findById(data.orderId);
+    const order = await Order.findById(data.orderId);
     if (!order) {
       throw new Error('Order not found');
     }
     order.set({
       status: OrderStatus.Complete,
     });
+
+    await order.save();
 
     msg.ack();
   }
