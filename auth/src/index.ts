9a597a3ca712589ec581d0f53b2facf80e070a03
Debabrata <m.debabrata100@gmail.com>
2025-12-12 19:04:25 +0530

    Auth deploy test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('AUTH starting up...');
+  console.log('AUTH starting up....');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
